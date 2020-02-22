56c3f0c7f11ebcf18c2f657314d68a0184e44cc5
ernestbies <ernestbies@gmail.com>
2020-02-22 14:04:50 +0100

    Added files

---
+++ src/databaseanalyzer/DatabaseAnalyzer.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databaseanalyzer;
+
+import gui.DatabaseGUI;
+
+/**
+ *
+ * @author Ernest Bieś, PWSZ Tarnów 2019
+ */
+public class DatabaseAnalyzer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DatabaseGUI gui = new DatabaseGUI();
+        gui.setVisible(true);
+    }
+    
+}
